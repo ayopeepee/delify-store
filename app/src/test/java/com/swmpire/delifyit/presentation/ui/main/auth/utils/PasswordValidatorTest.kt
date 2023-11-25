afe6a7f4d92309cabbb03c8488a17bc654a648c8
ayopeepee <topdenmax@mail.ru>
2023-11-25 13:20:21 +0300

    add unit tests for password validation

---
+++ app/src/test/java/com/swmpire/delifyit/presentation/ui/main/auth/utils/PasswordValidatorTest.kt
@@ -0,0 +1,80 @@
+package com.swmpire.delifyit.presentation.ui.main.auth.utils
+
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+
+class PasswordValidatorTest {
+
+    private val mockText: TextInputEditText = mockk(relaxed = true)
+    private val mockLayout: TextInputLayout = mockk(relaxed = true)
+
+    @Test
+    fun `test correct password validation`() {
+        // arrange
+        val password = "123456"
+        every { mockText.text.toString() } returns password
+
+        // act
+        val actual = PasswordValidator.validate(mockText, mockLayout)
+
+        // assert
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun `test incorrect (empty) password validation`() {
+        // arrange
+        val password = ""
+        every { mockText.text.toString() } returns password
+
+        // act
+        val actual = PasswordValidator.validate(mockText, mockLayout)
+
+        // assert
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun `test incorrect (contains spaces) password validation`() {
+        // arrange
+        val password = " "
+        every { mockText.text.toString() } returns password
+
+        // act
+        val actual = PasswordValidator.validate(mockText, mockLayout)
+
+        // assert
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun `test incorrect (less than 6 symbols) password validation`() {
+        // arrange
+        val password = "123"
+        every { mockText.text.toString() } returns password
+
+        // act
+        val actual = PasswordValidator.validate(mockText, mockLayout)
+
+        // assert
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun `test incorrect (more that 20 symbols) password validation`() {
+        // arrange
+        val password = "12345678912345678912345"
+        every { mockText.text.toString() } returns password
+
+        // act
+        val actual = PasswordValidator.validate(mockText, mockLayout)
+
+        // assert
+        assertThat(actual).isFalse()
+    }
+}
